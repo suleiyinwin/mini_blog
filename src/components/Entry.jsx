4335ea872afe263e3eeeb9662a526ed584718fb2
Su Lei Yin Win <yunluo.2106@gmail.com>
2023-05-05 21:05:53 +0700

    entry second commit

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,33 +1,44 @@
-import { Block } from "@mui/icons-material";
-import { AppBar, Box, Button, Modal, TextField, Toolbar, Typography } from "@mui/material";
+import { Block, Label } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Modal,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material";
 import Category from "./Category";
 import Post from "./Post";
 import Grid from "@mui/material/Unstable_Grid2";
-import * as React from 'react';
+import * as React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function Entry() {
   const [open, setOpen] = React.useState(false);
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const handleOpen = () => {
     setOpen(true);
-};
+  };
   const handleClose = () => {
     setOpen(false);
-};
-const handleUsernameChange = (event) => {
-  setUsername(event.target.value);
-};
+  };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-const handlePasswordChange = (event) => {
-  setPassword(event.target.value);
-};
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-const handleLogin = () => {
-  // Handle login logic here
-};
-
+  const handleLogin = () => {
+    // Handle login logic here
+  };
 
   const cat = [
     "Technology",
@@ -91,6 +102,25 @@ const handleLogin = () => {
     textTransform: "none",
     padding: "10px 20px 10px 20px",
   };
+  const signButton = {
+    backgroundColor: "#023047",
+    "&:hover": {
+      //you want this to be the same as the backgroundColor above
+      backgroundColor: "#023047",
+    },
+    width: { md: "60%", xs: "80%" },
+    textTransform: "none",
+    padding: "10px 0",
+    fontFamily: "arial",
+    fontSize: { xs: "14px", md: "16px" },
+    alignItems: "center",
+    marginLeft: { md: "20%", xs: "10%" },
+  };
+  const signField = {
+    width: { md: "60%", xs: "80%" },
+    fontFamily: "arial",
+    marginLeft: { md: "20%", xs: "10%" },
+  };
 
   const entry = {
     padding: "6%",
@@ -104,18 +134,39 @@ const handleLogin = () => {
     borderBottom: 1,
     borderColor: "#b0b0b0",
   };
-  const modalStyle={
-    position: 'absolute',
-    bgcolor: 'white',
-    height: {xs: "100%", md:"80%"},
-    width: {xs:'100%',md:"60%"},
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    alignItems:"center",
-    justifyContent:"center",
-    direction:'column'
-  }
+  const modalStyle = {
+    position: "absolute",
+    bgcolor: "white",
+    height: { xs: "100%", md: "80%" },
+    width: { xs: "100%", md: "60%" },
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    alignItems: "center",
+    justifyContent: "center",
+    direction: "column",
+  };
+  const headerTextStyle = {
+    fontFamily: "Roboto",
+    fontSize: { xs: "20px", md: "30px" },
+  };
+  const [isChecked, setIsChecked] = React.useState(false);
+  const [hasError, setHasError] = React.useState(false);
+
+  const handleCheck = (event) => {
+    setIsChecked(event.target.checked);
+    setHasError(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!isChecked) {
+      setHasError(true);
+    } else {
+      // submit form
+    }
+  };
+
   return (
     <Box>
       <ThemeProvider theme={theme}>
@@ -129,39 +180,75 @@ const handleLogin = () => {
               <img src="./assets/logo1.svg" />
             </Box>
             <Box style={{ padding: "0 5% 0 0" }}>
-              <Button sx={chips} onClick={handleOpen}>Get Started</Button>
-              <Modal open={open}
+              <Button sx={chips} onClick={handleOpen}>
+                Get Started
+              </Button>
+              <Modal
+                open={open}
                 onClose={handleClose}
                 // aria-labelledby="login-modal-title"
                 // aria-describedby="login-modal-description"
-                >
+              >
                 <Box sx={modalStyle}>
-                  <Typography variant= theme={theme1} style={{textAlign:"center"}}>Join Us</Typography>
-          {/* <p id="login-modal-description">Enter your credentials below to login.</p> */}
-          <form>
-            <TextField
-              style={{width:"60%", marginLeft:"20%"}}
-              label="Username"
-              value={username}
-              onChange={handleUsernameChange}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-            />
-            <TextField
-              style={{width:"60%", marginLeft:"20%"}}
-              label="Password"
-              type="password"
-              value={password}
-              onChange={handlePasswordChange}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-            />
-            <Button variant="contained" color="primary" onClick={handleLogin}>
-              Login
-            </Button>
-          </form>
+                  <IconButton onClick={handleClose}>
+                    <CloseIcon />
+                  </IconButton>
+                  <Typography
+                    sx={headerTextStyle}
+                    style={{ textAlign: "center" }}
+                  >
+                    Join Us
+                  </Typography>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      sx={signField}
+                      style={{ backgroundColor: "#F0F0F0" }}
+                      label="Username"
+                      value={username}
+                      onChange={handleUsernameChange}
+                      fullWidth
+                      margin="normal"
+                      // variant="outlined"
+                    />
+                    <TextField
+                      sx={signField}
+                      label="Password"
+                      type="password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                      fullWidth
+                      margin="normal"
+                      // variant="outlined"
+                    />
+                    <br></br>
+                    <br />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={isChecked}
+                          onChange={handleCheck}
+                          color="primary"
+                        />
+                      }
+                      label="By creating an account you agree to the terms of use and our privacy and policy"
+                      sx={signField}
+                    />
+                    <Box sx={signField}>
+                      {hasError && (
+                        <div style={{ color: "red" }}>Please check the box</div>
+                      )}
+                    </Box>
+                    <br />
+                    <br />
+                    <Button onClick={handleLogin} sx={signButton}>
+                      Login
+                    </Button>
+                    <br />
+                    <br />
+                    <Box sx={signField}>
+                      <p>Have already an account?</p>
+                    </Box>
+                  </form>
                 </Box>
               </Modal>
             </Box>
